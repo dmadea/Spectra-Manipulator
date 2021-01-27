02c3ad0ef5b944f56cb1e2a8cb06d6977b40db81
dmadea <dominik.madea@gmail.com>
2021-01-27 16:22:43 +0100

    comment removed

--- spectramanipulator/user_namespace.py
+++ spectramanipulator/user_namespace.py
@@ -798,67 +798,6 @@ def plot_fits(data_group, fit_group, residuals_group, n_rows=None, n_cols=None, 
 
     plt.show()
 
-#
-# def plot_fit(data_item, fit_item, residuals_item, symlog=False, linscale=1, linthresh=100,
-#                   lw_data=0.5, lw_fit=1.5, fig_size=(5, 4), y_label='$\\Delta$A', x_label='Time / $\\mu$s',
-#                   x_lim=(None, None), t_mul_factor=1, y_lim=(None, None), filepath=None, dpi=500, transparent=False,
-#              x_major_formatter=ScalarFormatter(), x_major_locator=None, y_major_locator=None, data_color='red'):
-#
-#     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=fig_size, gridspec_kw={'height_ratios': (4, 1)})
-#
-#     t_data = data_item.data[:, 0] * t_mul_factor
-#
-#     _x_lim = list(x_lim)
-#     _y_lim = list(y_lim)
-#
-#     _x_lim[0] = data_item.data[0, 0] if _x_lim[0] is None else _x_lim[0]
-#     _x_lim[1] = data_item.data[-1, 0] if _x_lim[1] is None else _x_lim[1]
-#
-#     _x_lim[0] *= t_mul_factor
-#     _x_lim[1] *= t_mul_factor
-#
-#     _y_lim[0] = data_item.data[:, 1].min() if _y_lim[0] is None else _y_lim[0]
-#     _y_lim[1] = data_item.data[:, 1].max() if _y_lim[1] is None else _y_lim[1]
-#
-#     _set_main_axis(ax1, x_label="", y_label=y_label, xlim=_x_lim, ylim=_y_lim, x_major_locator=x_major_locator,
-#                    y_major_locator=y_major_locator)
-#     _set_main_axis(ax2, x_label=x_label, y_label='res.', xlim=_x_lim, x_minor_locator=None, y_minor_locator=None)
-#
-#     # plot zero lines
-#     ax1.axline((0, 0), slope=0, ls='--', color='black', lw=0.5)
-#     ax2.axline((0, 0), slope=0, ls='--', color='black', lw=0.5)
-#
-#     ax1.plot(t_data, data_item.data[:, 1], lw=lw_data, color=data_color)
-#     ax1.plot(fit_item.data[:, 0] * t_mul_factor, fit_item.data[:, 1], lw=lw_fit, color='black')
-#     ax2.plot(residuals_item.data[:, 0] * t_mul_factor, residuals_item.data[:, 1], lw=lw_data, color=data_color)
-#
-#     ax1.set_axisbelow(False)
-#     ax2.set_axisbelow(False)
-#
-#     ax1.yaxis.set_ticks_position('both')
-#     ax1.xaxis.set_ticks_position('both')
-#
-#     ax2.yaxis.set_ticks_position('both')
-#     ax2.xaxis.set_ticks_position('both')
-#
-#     if symlog:
-#         ax1.set_xscale('symlog', subs=[2, 3, 4, 5, 6, 7, 8, 9], linscale=linscale, linthresh=linthresh)
-#         ax2.set_xscale('symlog', subs=[2, 3, 4, 5, 6, 7, 8, 9], linscale=linscale, linthresh=linthresh)
-#         ax1.xaxis.set_minor_locator(MinorSymLogLocator(linthresh))
-#         ax2.xaxis.set_minor_locator(MinorSymLogLocator(linthresh))
-#
-#     if x_major_formatter:
-#         ax1.xaxis.set_major_formatter(x_major_formatter)
-#         ax2.xaxis.set_major_formatter(x_major_formatter)
-#
-#     plt.tight_layout()
-#
-#     if filepath:
-#         ext = os.path.splitext(filepath)[1].lower()[1:]
-#         plt.savefig(fname=filepath, format=ext, transparent=transparent, dpi=dpi)
-#
-#     plt.show()
-
 
 def reaction_QY_relative(sample_items, actinometer_items, QY_act=1, irradiation_spectrum=None,
                          irradiation_wavelength=None, integration_range=(None, None), V_solution=1,
